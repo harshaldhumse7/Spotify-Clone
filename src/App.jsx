93977ceeb709e267dbb3b8409eb57d6575a71218
Harshal-007 <harshaldhumse@gmail.com>
2024-12-28 16:59:17 +0530

    Spotify-Clone2

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import Sidebar from './components/sidebar'
 import Player from './components/Player'
 import Display from './components/Display'
 
+
 const App = () => {
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebar />
         <Display />
+        
       </div>
       <Player/>
+      
 
     </div>
   )
